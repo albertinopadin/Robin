2032a6d59e7e1d34da078b28c60e64ad39835cc0
Albertino Padin Perez <albertinopadin@hotmail.com>
2022-07-13 01:31:04 -0700

    Cleaned up.

--- Robin/Form1.cs
+++ Robin/Form1.cs
@@ -17,6 +17,8 @@ namespace Robin
 {
     public partial class RobinForm : Form
     {
+        const string baseFilePath = @"C:\Users\albertinopadin\Videos\";
+
         public RobinForm()
         {
             InitializeComponent();
@@ -24,40 +26,20 @@ namespace Robin
 
         private async void btn_download_Click(object sender, EventArgs e)
         {
-            //var yt = YouTube.Default;
             var yt = new FastYouTube();
-            //var videos = yt.GetAllVideos(textBox_videoURL.Text);
             var videos = yt.GetAllVideosAsync(textBox_videoURL.Text).GetAwaiter().GetResult();
 
-            //var video = videos.First(v => v.Resolution == videos.Max(j => j.Resolution));
-            var video = yt.GetVideo(textBox_videoURL.Text);
-            label_videoTitle.Text = video.Title;
-            label_videoExtension.Text = video.FileExtension;
-            label_videoResolution.Text = video.Resolution.ToString();
-            label_maxBitrate.Text = video.AudioBitrate.ToString();
+            var videosWithAudio = videos.Where(v => v.Resolution > 0 && v.AudioBitrate > 0)
+                                        .OrderByDescending(t => t.Resolution)
+                                        .ToList();
 
-            var maxRes = videos.First(v => v.Resolution == videos.Max(m => m.Resolution));
-            var maxBR = videos.First(v => v.AudioBitrate == videos.Max(m => m.AudioBitrate));
+            var maxResWithAudio = videosWithAudio.First();
 
-            Console.WriteLine($"Max resolution video -> Resolution: {maxRes.Resolution}, " +
-                              $"Audio Bit Rate: {maxRes.AudioBitrate}");
+            label_videoTitle.Text = maxResWithAudio.Title;
+            label_videoExtension.Text = maxResWithAudio.FileExtension;
+            label_videoResolution.Text = maxResWithAudio.Resolution.ToString();
+            label_maxBitrate.Text = maxResWithAudio.AudioBitrate.ToString();
 
-            Console.WriteLine($"Max audio bit rate video -> Resolution: {maxBR.Resolution}, " +
-                              $"Audio Bit Rate: {maxBR.AudioBitrate}");
-
-            var maxResWithBR = videos.FirstOrDefault(v => v.AudioBitrate > 0 && v.Resolution == videos.Max(m => m.Resolution));
-            if (maxResWithBR != null)
-            {
-                Console.WriteLine($"Max resolution video with Audio BR -> Resolution: {maxResWithBR.Resolution}, " +
-                                  $"Audio Bit Rate: {maxResWithBR.AudioBitrate}");
-            } else
-            {
-                Console.WriteLine("There is no video with max resolution AND audio bitrate!");
-            }
-
-            var videosWithAudio = videos.Where(v => v.Resolution > 0 && v.AudioBitrate > 0).ToList();
-
-            Console.WriteLine("VIdeos with Audio:");
             foreach (var vi in videosWithAudio)
             {
                 Console.WriteLine("Video Info: ");
@@ -68,64 +50,28 @@ namespace Robin
                 Console.WriteLine("\n");
             }
 
-            var baseFilePath = @"C:\Users\albertinopadin\Videos\";
-            await downloadVideo(baseFilePath, video, yt);
+            await downloadVideo(yt,
+                                maxResWithAudio,
+                                baseFilePath,
+                                new Progress<Tuple<long, long>>((Tuple<long, long> v) =>
+                                {
+                                    var percent = (int)((v.Item1 * 100) / v.Item2);
+                                    progressBarDownload.Value = percent;
+                                    progressBarDownload.Update();
+                                }));
         }
 
-        private async Task downloadVideo(string downloadFolder, YouTubeVideo video, FastYouTube youTube)
+        private async Task downloadVideo(FastYouTube youTube, 
+                                         YouTubeVideo video, 
+                                         string downloadFolder,
+                                         IProgress<Tuple<long, long>> progress)
         {
             Console.WriteLine("Download Started");
             await youTube.CreateDownloadAsync(
                 new Uri(video.Uri),
                 Path.Combine(downloadFolder, video.FullName),
-                new Progress<Tuple<long, long>>((Tuple<long, long> v) =>
-                {
-                    var percent = (int)((v.Item1 * 100) / v.Item2);
-                    //Console.WriteLine("Progress: " + percent);
-                    progressBarDownload.Value = percent;
-                    progressBarDownload.Update();
-                }));
+                progress);
             Console.WriteLine("Download Complete");
-
-            /*
-            long? totalBytes = 0;
-            var httpClientHandler = new HttpClientHandler()
-            {
-                MaxResponseHeadersLength = 64
-            };
-
-            var client = new HttpClient(httpClientHandler);
-            progressBarDownload.Minimum = 0;
-            progressBarDownload.Maximum = 100;
-
-            using (Stream output = File.OpenWrite(downloadFolder + video.FullName))
-            {
-                using (var request = new HttpRequestMessage(HttpMethod.Head, video.Uri))
-                {
-                    totalBytes =
-                        client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).Result.Content.Headers.ContentLength;
-                }
-
-                using (var input = await client.GetStreamAsync(video.Uri))
-                {
-                    byte[] buffer = new byte[256 * 1024];
-                    int read;
-                    int totalRead = 0;
-                    Console.WriteLine("Download Started");
-                    while ((read = await input.ReadAsync(buffer, 0, buffer.Length)) > 0)
-                    {
-                        output.Write(buffer, 0, read);
-                        totalRead += read;
-                        var progress = (int)(((double)totalRead / (double)totalBytes) * 100);
-                        progressBarDownload.Value = progress;
-                        progressBarDownload.Update();
-                        Console.WriteLine("Read: " + read);
-                        Console.WriteLine("Total read: " + totalRead);
-                        Console.WriteLine("Progress: " + progress);
-                    }
-                    Console.WriteLine("Download Complete");
-                }
-            } */
         }
     }
 }
